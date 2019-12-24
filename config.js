8ede75b04df530ac35cf7cace5c9f3690a5e6827
Ruslan <azimut.nt@gmail.com>
2019-12-25 02:12:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const QUERY_PARAMS = require('./queryParams.json');
 const config = {
     pollPeriodMinutes: 5,
     queryParams: QUERY_PARAMS,
-    botToken: '383953254:AAFVT2ay9VFlgjixMCHWGDhKxYrtOK-IF1c',
+    botToken: 'YOURTOKEN',
 };
 
 module.exports = config;
